e6a698d7feaf5cd26cc858773d62d412aad7ba5d
Jonathon Harrell <jonathonharrell@yahoo.com>
2015-04-28 10:48:01 -0500

    Exception doc block

--- src/RemoteResource/Exception.php
+++ src/RemoteResource/Exception.php
@@ -1,12 +1,22 @@
 <?php
 namespace RemoteResource;
 
+/**
+ * RemoteResource's custom exception class, needed a method to store the response
+ */
 class Exception extends \Exception {
   public $response;
 
+  /**
+   * The exception stores the response for reference when handling errors
+   * @param mixec          $response the response object
+   * @param string         $message  the exception message
+   * @param integer        $code     an error code
+   * @param Exception|null $previous usually null
+   */
   public function __construct($response, $message = "", $code = 0, Exception $previous = null) {
     parent::__construct($message, $code, $previous);
 
-    $this->response = $response; 
+    $this->response = $response;
   }
 }
