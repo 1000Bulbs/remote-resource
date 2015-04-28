84c5c7122a98c7aa77092152299e4a07ac33d515
Jonathon Harrell <jonathonharrell@yahoo.com>
2015-04-28 10:53:29 -0500

    More doc blocks

--- src/RemoteResource/Formatter.php
+++ src/RemoteResource/Formatter.php
@@ -1,7 +1,24 @@
 <?php
+
 namespace RemoteResource;
 
+/**
+ * Formatters must implement this interface
+ */
 interface Formatter {
+
+  /**
+   * Format a request body
+   * @param  array $request_body
+   * @return string               formatted request body
+   */
   public function formatRequest($request_body);
+
+  /**
+   * Format response body
+   * @param  string $response_body
+   * @return array                 formatted response body
+   */
   public function formatResponse($response_body);
+
 }
--- src/RemoteResource/Pool.php
+++ src/RemoteResource/Pool.php
@@ -1,6 +1,17 @@
 <?php
+
 namespace RemoteResource;
 
+/**
+ * Resource Pools must implement this interface
+ */
 interface Pool {
+  
+  /**
+   * Get instance specific to $class_name
+   * @param  string $class_name
+   * @return mixed
+   */
   public static function getInstance( $class_name );
+
 }
