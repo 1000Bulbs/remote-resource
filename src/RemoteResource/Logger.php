cd32ea25251a4f70b516c28a7980664cdbb94cbe
daniel-jerome-long <daniel@danieljlong.com>
2015-06-08 10:53:30 -0500

    simplify logger

--- src/RemoteResource/Logger.php
+++ src/RemoteResource/Logger.php
@@ -17,59 +17,55 @@ class Logger {
   }
 
   public function warning(string $msg) {
-    if (!$this->available()) {
+    if ($this->available()) {
       $this->log->addWarning($msg);
     }
   }
 
   public function error(string $msg) {
-    if (!$this->available()) {
+    if ($this->available()) {
       $this->log->addError($msg);
     }
   }
 
   public function info(string $msg) {
-    if (!$this->available()) {
+    if ($this->available()) {
       $this->log->addInfo($msg);
     }
   }
 
   public function debug(string $msg) {
-    if (!$this->available()) {
+    if ($this->available()) {
       $this->log->addDebug($msg);
     }
   }
 
   public function notice(string $msg) {
-    if (!$this->available()) {
+    if ($this->available()) {
       $this->log->addNotice($msg);
     }
   }
 
   public function critical(string $msg) {
-    if (!$this->available()) {
+    if ($this->available()) {
       $this->log->addCritical($msg);
     }
   }
 
   public function alert(string $msg) {
-    if (!$this->available()) {
+    if ($this->available()) {
       $this->log->addAlert($msg);
     }
   }
 
   public function emergency(string $msg) {
-    if (!$this->available()) {
+    if ($this->available()) {
       $this->log->addEmergency($msg);
     }
   }
 
   private function available() {
-    if (!isset($this->log)) {
-      return false;
-    }
-
-    return true;
+    return $this->log ? true : false;
   }
 
 }
