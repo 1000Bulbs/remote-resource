eb5b6145e5d02f9ad8a8a7e751c7802db1f944a3
daniel-jerome-long <daniel@danieljlong.com>
2015-06-10 14:28:15 -0500

    set curl forbid reuse to true

--- src/RemoteResource/Connection.php
+++ src/RemoteResource/Connection.php
@@ -81,7 +81,7 @@ class Connection {
   public function client() {
     if (!$this->client) {
       $client = new Client;
-      $client->getConfig()->set('curl.options', array(CURLOPT_TCP_NODELAY => 1));
+      $client->getConfig()->set('curl.options', array(CURLOPT_TCP_NODELAY => 1, CURLOPT_FORBID_REUSE => 1));
       $client->setDefaultOption('exceptions', false);
       $this->client = $client;
     }
