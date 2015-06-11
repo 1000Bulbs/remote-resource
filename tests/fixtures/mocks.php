105b118d17e0299d1f151549ccc9144eb60984f2
daniel-jerome-long <daniel@danieljlong.com>
2015-06-11 12:36:52 -0500

    Revert "replace guzzle with requests"
    This reverts commit 04041224c1487e5d1ae74e111ef84becea058cea.

--- tests/fixtures/mocks.php
+++ tests/fixtures/mocks.php
@@ -14,22 +14,15 @@ class MockResponse {
 
 class MockClient {
   private $response;
-  public $body, $status_code;
 
   public function createRequest($verb, $path, $headers, $body) {
-    return $this;
-  }
-
-  public function request($verb, $path, $headers, $body) {
     return $this;
   }
 
   public function setResponseParams($status_code, $body=array()) {
     $this->response = new MockResponse();
     $this->response->status_code = $status_code;
-    $this->status_code = $status_code;
     $this->response->body = json_encode($body);
-    $this->body = json_encode($body);
   }
 
   public function send() {
